6257ab16d24a2fa9f26a4b66949b3d421129e074
gracekc <gracekc@users.noreply.github.com>
2016-06-21 10:51:33 -0700

    Update perfmatters.js

--- Web Optim. (4)/js/perfmatters.js
+++ Web Optim. (4)/js/perfmatters.js
@@ -1,6 +1,14 @@
 // Measuring the Critical Rendering Path with Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
+$> cd /path/to/your-project-folder
+  $> python -m SimpleHTTPServer 8080
+
+
+$> cd /path/to/your-project-folder
+$> ./ngrok http 8080
+
+
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
